0d4978b23255a810a2930ff900d28142c61c911c
Franco Marquez <fmarquez@deloitte.com>
2018-08-03 13:36:37 -0500

    add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { UserService } from  './user.service';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
